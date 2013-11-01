694ce7228940bc00a04d638cc2db3cad6f18eca2
pavelKoltsov <pkoltsov@gmail.com>
2013-10-31 22:01:49 -0500

    Original version

---
+++ Final635/src/Component.java
@@ -0,0 +1,19 @@
+public abstract class Component {
+
+	int benchmarkScore;
+	String description;
+	static int maxScore;
+	int tier;
+	float rating;
+
+	public abstract String getDescription();
+
+	public abstract float getRating();
+
+	public abstract void setMaxScore(int maxScore);
+
+	public void setRating() {
+		rating = (float) benchmarkScore / maxScore * 100;
+	}
+
+}
