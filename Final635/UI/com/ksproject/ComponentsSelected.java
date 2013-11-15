581df464cf662646a338bc894b0337de0fe8a008
pavelKoltsov <pkoltsov@gmail.com>
2013-11-15 17:01:51 -0600

    GUI update

---
+++ Final635/UI/com/ksproject/ComponentsSelected.java
@@ -0,0 +1,46 @@
+package com.ksproject;
+
+public class ComponentsSelected 
+{
+	
+	int hardDriveSelect;
+	int chipSetSelect;
+	int memorySelect;
+	
+	
+	int processorSelect;
+	public int getProcessorSelect() 
+	{
+		return processorSelect;
+	}
+	public void setProcessorSelect(int processorSelect) 
+	{
+		this.processorSelect = processorSelect;
+	}
+	public int getHardDriveSelect() 
+	{
+		return hardDriveSelect;
+	}
+	public void setHardDriveSelect(int hardDriveSelect) 
+	{
+		this.hardDriveSelect = hardDriveSelect;
+	}
+	public int getChipSetSelect() 
+	{
+		return chipSetSelect;
+	}
+	public void setChipSetSelect(int chipSetSelect) 
+	{
+		this.chipSetSelect = chipSetSelect;
+	}
+	public int getMemorySelect() 
+	{
+		return memorySelect;
+	}
+	public void setMemorySelect(int memorySelect) 
+	{
+		this.memorySelect = memorySelect;
+	}
+
+	
+}
